b822ce14a1cc6c50e081118bce0b7da7149c6670
Dominik Facher <dfacher@gmail.com>
2013-07-07 14:39:57 +0000

    web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     var buffer = new Buffer(16);
     fs.readSync('index.html', buffer,0,16,0);
-    response.send(buffer);
+    response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
